3d36b8dbf258991d6ea7e8ec398d6de737525dc0
chienvu94 <chienvv@massgenie.com>
2020-10-13 18:13:58 +0700

    Env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -15,10 +18,10 @@ app.get('/api/product/:id', (req, res) => {
     res.json(product)
 })
 
-
+const PORT = process.env.PORT || 5000
 app.listen(
-    5000,
+    PORT,
     console.log(
-      'Server running on port 5000'
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
 )
