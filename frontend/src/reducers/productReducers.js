54eb3b8462141d858f6c99507257d940b631688a
chienvu94 <chienvv@massgenie.com>
2020-10-14 16:22:08 +0700

    Product Detail Page

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,11 @@
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+    
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -19,3 +23,19 @@ export const productListReducer = (state = { products: [] }, action) => {
       return state;
   }
 };
+
+export const productDetailsReducer = (
+    state = { product: { reviews: [] } },
+    action
+  ) => {
+    switch (action.type) {
+      case PRODUCT_DETAILS_REQUEST:
+        return { ...state, loading: true }
+      case PRODUCT_DETAILS_SUCCESS:
+        return { loading: false, product: action.payload }
+      case PRODUCT_DETAILS_FAIL:
+        return { loading: false, error: action.payload }
+      default:
+        return state
+    }
+  }
