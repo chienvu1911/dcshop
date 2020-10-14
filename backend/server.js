e490171dc1bfa2ad25c7ba223d88e4473202db75
chienvu94 <chienvv@massgenie.com>
2020-10-14 15:02:39 +0700

    Handle error

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db.js')
-const products = require('./data/products')
+const productRoutes = require('./routes/productRoutes.js')
 
 dotenv.config()
 connectDB()
@@ -11,14 +12,15 @@ app.get('/', (req, res) => {
     res.send('API is running....')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
 app.get('/api/product/:id', (req, res) => {
     const product = products.find(product => product._id === req.params.id);
     res.json(product)
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
